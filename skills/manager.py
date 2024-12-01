ceb2c496e9c46e5fcb43c28e3109c0a8c157a15a
ignacy kurowski <ignacykurowski@tutanota.com>
2024-12-01 01:16:12 +0100

    skills moved into skills.py script

--- skills/manager.py
+++ skills/manager.py
@@ -2,8 +2,8 @@ import utils
 import random
 
 from config import SKILLS, DamageType, ActorStatusType, StatType, AffType
-SKILLS = SKILLS
 from affects import manager as aff_manager
+import skills.skills 
 
 def get_skills():
     name_to_id = {}
@@ -108,9 +108,10 @@ def use_skill(user: "Actor", target: "Actor", skill_name: str, used_from_consuma
         skill_id = best_match
         cooldown = SKILLS[best_match]['cooldown']
 
-        script = globals()['Skill'+str(SKILLS[best_match]['script_to_run'])](skill_id = best_match, cooldown = cooldown, user = user, other = target, users_skill_level = users_skill_level, use_perspectives = use_perspectives, success = success, silent_use = silent_use)
-        script.use()
-        del script
+        skill_obj = getattr(skills.skills,f'Skill{SKILLS[best_match]["script_to_run"]}')
+        skill_obj = skill_obj(skill_id = best_match, cooldown = cooldown, user = user, other = target, users_skill_level = users_skill_level, use_perspectives = use_perspectives, success = success, silent_use = silent_use)
+        skill_obj.use()
+        del skill_obj
 
         return True
     except Exception as e:
@@ -119,107 +120,3 @@ def use_skill(user: "Actor", target: "Actor", skill_name: str, used_from_consuma
 
 
 
-class Skill:
-    def __init__(self, skill_id, cooldown, user, other, users_skill_level: int, use_perspectives, success, silent_use = False):
-        self.skill_id = skill_id
-        self.cooldown = cooldown
-        self.user = user
-        self.other = other
-        self.users_skill_level = users_skill_level
-        self.use_perspectives = use_perspectives
-        self.success = success
-        self.silent_use = silent_use
-
-
-    def use_broadcast(self):
-        perspectives = {
-            'you on you':       self.use_perspectives['you on you fail'],
-            'you on other':     self.use_perspectives['you on other fail'],
-            'user on user':     self.use_perspectives['user on user fail'],
-            'user on you':      self.use_perspectives['user on you fail'],
-            'user on other':    self.use_perspectives['user on other fail']
-        }
-        if self.success:
-            perspectives = {
-                'you on you':       self.use_perspectives['you on you'],
-                'you on other':     self.use_perspectives['you on other'],
-                'user on user':     self.use_perspectives['user on user'],
-                'user on you':      self.use_perspectives['user on you'],
-                'user on other':    self.use_perspectives['user on other']
-            }
-
-        for perspective in perspectives:
-            perspectives[perspective] = perspectives[perspective].replace('#USER#', self.user.pretty_name())
-            perspectives[perspective] = perspectives[perspective].replace('#OTHER#', self.other.pretty_name())
-
-        for receiver in self.user.room.entities.values():
-            if type(receiver).__name__ != "Player":
-                continue
-
-            if receiver == self.user and receiver == self.other:
-                receiver.sendLine(perspectives['you on you'])
-                continue
-            if receiver == self.user and receiver != self.other:
-                receiver.sendLine(perspectives['you on other'])
-                continue
-            if receiver != self.user and receiver != self.other and self.user == self.other:
-                receiver.sendLine(perspectives['user on user'])
-                continue
-            if receiver != self.user and receiver == self.other:
-                receiver.sendLine(perspectives['user on you'])
-                continue
-            if receiver != self.user and receiver != self.other:
-                receiver.sendLine(perspectives['user on other'])
-                continue
-
-    def use(self):
-        if self.silent_use:
-            return
-        self.user.cooldown_manager.add_cooldown(self.skill_id, self.cooldown)
-        self.use_broadcast()
-
-class SkillSwing(Skill):
-    def use(self):
-        super().use()
-        if self.success:
-            self.other.take_damage(self.user, 1 + self.user.stats[StatType.BODY], DamageType.PHYSICAL)
-
-class SkillCureLightWounds(Skill):
-    def use(self):
-        super().use()
-        if self.success:
-            self.other.take_damage(self.user, 10 * self.users_skill_level, DamageType.HEALING)
-
-class SkillBash(SkillSwing):
-    def use(self):
-        super().use()
-        if self.success:
-            stunned_affect = aff_manager.AffectStunned(
-                AffType.STUNNED,
-                self.other.affect_manager,
-                'Stunned', 'Unable to act during combat turns',
-                turns = 3
-            )
-            self.other.affect_manager.set_affect_object(stunned_affect)
-
-class SkillMagicMissile(Skill):
-    def use(self):
-        super().use()
-        if self.success:
-            self.other.take_damage(self.user, self.users_skill_level + self.user.stats[StatType.MIND], DamageType.MAGICAL)
-
-class SkillBecomeEthereal(Skill):
-    def use(self):
-        try:
-            super().use()
-            if self.success:
-                turns = 4 + int(self.users_skill_level*0.3)
-                dmg_amp = 2.4 - self.users_skill_level*0.1
-                ethereal_affect = aff_manager.AffectEthereal(
-                    AffType.ETHEREAL, 
-                    self.user.affect_manager, 
-                    'Ethereal', f'You take {int(dmg_amp*100)}% damage from spells, but are immune to physical damage', 
-                    turns, dmg_amp)
-                self.user.affect_manager.set_affect_object(ethereal_affect)
-        except Exception as e:
-            print(e)
