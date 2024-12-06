e68fa7a456091b8a4283935c6a8b70e6c4417a66
ignacy kurowski <ignacykurowski@tutanota.com>
2024-12-06 19:02:52 +0100

    working on docker client

--- tintin/start.sh
+++ tintin/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-firejail --private ttyd -p 8080 tt++ -e "#DELAY {1} {#SPLIT 0 0};" -e "#SESSION {3k} {127.0.0.1} {4001};" -e "#DELAY {1} {#SPLIT 0 0};"
+firejail --netfilter="" --private ttyd -p 8080 \ 
+tt++ -e "#DELAY {1} {#SPLIT 0 0};" -e "#SESSION {3k} {127.0.0.1} {4001};" -e "#DELAY {1} {#SPLIT 0 0};"
