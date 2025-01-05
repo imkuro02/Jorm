1baa89b3b340307862921d16328ebd0b9871f72c
ignacy kurowski <ignacykurowski@tutanota.com>
2025-01-05 14:52:00 +0100

    more admin commands

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-(source env/bin/activate && python3 .) & (cd client && source env/bin/activate && python3 .) & (cd client && python3 -m http.server 8110)
+(source env/bin/activate && python3 .) & (cd client && source env/bin/activate && python3 .) & (cd client && python3 -m http.server 80)
