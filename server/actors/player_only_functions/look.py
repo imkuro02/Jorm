c1697ff019a09d92c339547f86749e8e2da245c7
imkuro02 <ignacy@kurowski.xyz>
2025-02-07 12:33:28 +0100

    better map renderer

--- server/actors/player_only_functions/look.py
+++ server/actors/player_only_functions/look.py
@@ -5,17 +5,20 @@ def command_look(self, line):
 
     def draw_local_area(self, room_id):
 
-        VIEW_RANGE = 3
+        VIEW_RANGE = 4
+        START_LOC = f'{VIEW_RANGE},{VIEW_RANGE}'
 
         class Art:
             # all
-            GROUD =         '@yellow.'
+            GROUND =         '@yellow.'
+            EMPTY =         ' '
 
             # middle
             RECALL_SITE =   '@green+'
             PLAYER_HERE =   '@cyan@'
             SPECIAL_EXIT =  '@normal?'
             SPECIAL_EXIT_AND_PLAYER = '@cyan?'
+            RECALL_SITE_AND_PLAYER = '@cyan+'
             
             # right
             STAIRS_DOWN =   '@normal>'
@@ -24,23 +27,165 @@ def command_look(self, line):
             STAIRS_UP =     '@normal<'
             
         offsets = {
-            'north': [ 0  ,  -1 ],
-            'west':  [ -1 ,  0 ],
-            'south': [ 0  , 1 ],
-            'east':  [ 1  ,  0 ],
-            'left':  [ -1 ,  0 ],
-            'right': [ 1  ,  0 ],
+            'north': [ 0  ,  -2 ],
+            'west':  [ -2 ,  0 ],
+            'south': [ 0  , 2 ],
+            'east':  [ 2  ,  0 ],
+            #'left':  [ -2 ,  0 ],
+            #'right': [ 2  ,  0 ],
             # up and down need to FUCK OFF
             # otherwise they mess up and override other rooms they are on top of or under of
             'up':    [0,0+1000],
             'down':  [0,0+1000]
         }
 
+        offsets_path = {
+            'north': [ 0  ,  -1 ],
+            'west':  [ -1 ,  0 ],
+            'south': [ 0  , 1 ],
+            'east':  [ 1  ,  0 ],
+            #'left':  [ -1 ,  0 ],
+            #'right': [ 1  ,  0 ],
+            # up and down need to FUCK OFF
+            # otherwise they mess up and override other rooms they are on top of or under of
+            'up':    [0,0+1000],
+            'down':  [0,0+1000]
+        }
+
+        t = utils.Table((VIEW_RANGE*2)+1,0)
+
+
+        
+        grid = {}
+        start_room = self.protocol.factory.world.rooms[room_id]
+        grid[START_LOC] = room_id
+
+        for _exit in start_room.exits:
+            if _exit not in offsets:
+                continue
+            x = 0
+            y = 0
+            x += offsets[_exit][1] + VIEW_RANGE
+            y += offsets[_exit][0] + VIEW_RANGE
+            _loc = f'{x},{y}'
+            if _loc not in grid:
+                grid[_loc] = start_room.exits[_exit]
+            x += -offsets_path[_exit][1]
+            y += -offsets_path[_exit][0]
+            _loc = f'{x},{y}'
+            if _loc not in grid:
+                grid[_loc] = 'PATH'
+
+        _grid = {}
+        for r in grid:
+            _grid[r] = grid[r]
+            
+        for room_loc in _grid:
+            if grid[room_loc] == 'PATH':
+                continue
+            room = self.protocol.factory.world.rooms[grid[room_loc]]
+            _x = int(room_loc.split(',')[0])
+            _y = int(room_loc.split(',')[1])
+            for _exit in room.exits:
+                if _exit not in offsets:
+                    continue
+                x = _x
+                y = _y
+                x += offsets[_exit][1] 
+                y += offsets[_exit][0] 
+                _loc = f'{x},{y}'
+
+                grid[_loc] = room.exits[_exit]
+                x += -offsets_path[_exit][1]
+                y += -offsets_path[_exit][0]
+                _loc = f'{x},{y}'
+                if _loc not in grid:
+                    grid[_loc] = 'PATH'
+
+
+        print(grid)
+        for _x in range(0,(VIEW_RANGE*2)+1):
+            for _y in range(0,(VIEW_RANGE*2)+1):
+                """
+                cords = f'{_x},{_y}'
+                if cords not in grid:
+                    t.add_data('...','@red')
+                    continue
+                if grid[cords] == 'PATH':
+                    t.add_data('XXX')
+                    continue
+                if cords in grid:
+                    #t.add_data(grid[cords])
+                    t.add_data('!!!')
+                    continue
+                t.add_data('ERR','@red')
+                """
+                loc = f'{_x},{_y}'
+                
+                cell = ''
+
+                if loc not in grid:
+                    cell = Art.EMPTY*3
+                    t.add_data(cell)
+                    continue
+                
+                if grid[loc] == 'PATH':
+                    cell = ' '+Art.GROUND+' '
+                    t.add_data(cell)
+                    continue
+
+                room = self.protocol.factory.world.rooms[grid[loc]]
+                print(room)
+                # left
+                if 'up' in room.exits:
+                    cell += Art.STAIRS_UP
+                else:
+                    cell += Art.EMPTY
+                    
+                # mid
+                if 100 == 10:
+                    pass
+                elif room.can_be_recall_site and loc == START_LOC:
+                    cell += Art.RECALL_SITE_AND_PLAYER
+                elif len([ x for x in room.exits.keys() if x not in offsets.keys() ]) != 0 and loc == START_LOC: #set(offsets.keys()) - set(grid[loc].exits.keys()):
+                    cell += Art.SPECIAL_EXIT_AND_PLAYER
+                elif loc == START_LOC:
+                    cell += Art.PLAYER_HERE
+                elif room.can_be_recall_site:
+                    cell += Art.RECALL_SITE
+                elif len([ x for x in room.exits.keys() if x not in offsets.keys() ]) != 0:
+                    cell += Art.SPECIAL_EXIT
+                else:
+                    cell += Art.GROUND
+
+                # right
+                if 'down' in room.exits:
+                    cell += Art.STAIRS_DOWN
+                else:
+                    cell += Art.EMPTY
+
+                if grid[loc] == 'PATH':
+                    cell = ' '+Art.GROUND+' '
+                    t.add_data(cell)
+                    continue
+
+                t.add_data(cell)
+
+                
+                
+
+        output = t.get_table()
+
+            
+            
+
+        '''
         x = 0
         y = 0
         start_room = self.protocol.factory.world.rooms[room_id]
         loc = f'{x},{y}'
         grid = {}
+        path = {}
         grid[loc] = start_room
         cur_room = start_room
         mapped_rooms = []
@@ -54,32 +199,40 @@ def command_look(self, line):
                 _y += offsets[direction][1]
                 loc = f'{_x},{_y}'
                 grid[loc] = self.protocol.factory.world.rooms[cur_room.exits[direction]]
-                    
-        for _ in range(0,VIEW_RANGE):
-            _grid = {}
-            for loc in grid:
-                for direction in grid[loc].exits:
-                    if direction not in offsets:
-                        continue
+                print(loc)
 
-                    _x = x
-                    _y = y
-                    _x = int(loc.split(',')[0])
-                    _y = int(loc.split(',')[1])
+        _grid = {}
+        _path = {}
+        for loc in grid:
+            for direction in grid[loc].exits:
+                if direction not in offsets:
+                    continue
 
-                    _x += offsets[direction][0]
-                    _y += offsets[direction][1]
-                    _loc = f'{_x},{_y}'
-                    if _loc not in grid:
-                        _grid[_loc] = self.protocol.factory.world.rooms[grid[loc].exits[direction]]
+                _x = x
+                _y = y
+                _x = int(loc.split(',')[0])
+                _y = int(loc.split(',')[1])
 
-            
-            for _loc in _grid:
+                _x += offsets[direction][0]
+                _y += offsets[direction][1]
+                _loc = f'{_x},{_y}'
                 if _loc not in grid:
-                    grid[_loc] = _grid[_loc]
+                    _grid[_loc] = self.protocol.factory.world.rooms[grid[loc].exits[direction]]
+                    _x += offsets_path[direction][0]
+                    _y += offsets_path[direction][1]
+                    _loc = f'{_x},{_y}'
+                    _path[_loc] = True
+                   
+        print(_path)
+        for _loc in _grid:
+            if _loc not in grid:
+                grid[_loc] = _grid[_loc]
+
+        for _loc in _path:
+            if _loc not in path:
+                path[_loc] = _path[_loc]
             #_grid_ = grid | _grid
             #grid = _grid_
-        
 
 
             
@@ -92,11 +245,14 @@ def command_look(self, line):
             loc = f'{_x+VIEW_RANGE},{_y+VIEW_RANGE}'
             grid[loc] = _grid[_loc]       
 
-        '''
-        name_grid = {}
-        for i in grid:
-            name_grid[i] = grid[i].uid
-        '''
+        _path = path
+        path = {}
+        for _loc in _path:
+            _x += int(_loc.split(',')[0])
+            _y += int(_loc.split(',')[1])
+            loc = f'{_x+VIEW_RANGE},{_y+VIEW_RANGE}'
+            path[loc] = _path[_loc] 
+
 
         t = utils.Table((VIEW_RANGE*2)+1,0)
 
@@ -106,16 +262,19 @@ def command_look(self, line):
                 
                 cell = ''
                 
-                if loc not in grid:
+                if loc not in grid and loc not in path:
                     t.add_data('   ')
                     continue
 
-
+                if loc in path:
+                    t.add_data('---')
+                    continue
+                    
                 # left
                 if 'up' in grid[loc].exits:
                     cell += Art.STAIRS_UP
                 else:
-                    cell += Art.GROUD
+                    cell += Art.GROUND
                     
 
                 # mid
@@ -130,18 +289,19 @@ def command_look(self, line):
                 elif len([ x for x in grid[loc].exits.keys() if x not in offsets.keys() ]) != 0:
                     cell += Art.SPECIAL_EXIT
                 else:
-                    cell += Art.GROUD
+                    cell += Art.GROUND
 
                 # right
                 if 'down' in grid[loc].exits:
                     cell += Art.STAIRS_DOWN
                 else:
-                    cell += Art.GROUD
+                    cell += Art.GROUND
                 
                 #self.sendLine(f'{loc}, {grid[loc].exits.keys()}')
                 t.add_data(cell)
                
         output = t.get_table()
+        '''
         self.sendLine(output)
 
 
