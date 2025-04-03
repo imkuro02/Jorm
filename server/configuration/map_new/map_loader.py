f17114b7a995e4ebab269d154b7f0bf80514644d
imkuro02 <ignacy@kurowski.xyz>
2025-04-03 15:02:40 +0200

    pain

--- server/configuration/map_new/map_loader.py
+++ server/configuration/map_new/map_loader.py
@@ -4,6 +4,7 @@ def load_map():
     nodes = {}
     edges = {}
     all_data = {'nodes':{},'edges':{}}
+    
     MAP_DIRECTORY = 'configuration/map_new/'
     for root, dirs, files in os.walk(MAP_DIRECTORY):
         for filename in files:
@@ -16,12 +17,13 @@ def load_map():
                     all_data['edges'].update(data['edges'])
 
     for node in all_data['nodes'].values():
+        print(node)
         data =      node['data']
         json_data = node['data']['json']
 
         room_id = data['id']
-        room_name = data['name']
-        room_desc = 'no desc'
+        room_name = 'NO NAME'
+        room_desc = 'NO DESC'
         room_safe = False
         room_instanced = False
         room_npcs = []
@@ -48,14 +50,26 @@ def load_map():
             if skip_loading:
                 continue
 
+        if 'name' in json_data:
+            room_name = json_data['name']
+
         if 'description' in json_data:
-            room_desc = json_data['description']
+            if type(json_data['description']) == str:
+                room_desc = json_data['description']
+            if type(json_data['description']) == list:
+                room_desc = '\n'.join(json_data['description'])
 
         if 'npcs' in json_data:
             room_npcs= json_data['npcs']
 
         if 'items' in json_data:
             room_items = json_data['items']
+
+        if 'instanced' in json_data:
+            room_instanced = json_data['instanced']
+
+        if 'can_be_recall_site' in json_data:
+            room_safe = json_data['can_be_recall_site']
 
         nodes[room_id] = {
             'id': room_id,
@@ -71,20 +85,25 @@ def load_map():
 
     for edge in all_data['edges'].values():
         edge_data = edge['data']
+        json_data = edge_data['json']
+
         if edge_data['source'] in nodes:
             
             new_exit = {
                 'from': edge_data['source'], 
                 'to_room_id': edge_data['target'], 
-                'direction': edge_data['name'],
+                'direction': 'edge.data.json has no direction',
                 'secret': False,
                 'blocked': False
             }
 
-            if 'secret' in edge_data['json']:
-                new_exit['secret'] = edge_data['json']['secret']
-            if 'blocked' in edge_data['json']:
-                new_exit['blocked'] = edge_data['json']['secret']
+
+            if 'secret' in json_data:
+                new_exit['secret'] = json_data['secret']
+            if 'blocked' in json_data:
+                new_exit['blocked'] = json_data['blocked']
+            if 'direction' in json_data:
+                new_exit['direction'] = json_data['direction']
 
             nodes[edge_data['source']]['exits'].append(new_exit)
 
