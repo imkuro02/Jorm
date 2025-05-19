6d836e3aaded421054ba1285e5c4d1048c2c9d1d
ignacy kurowski <ignacykurowski@tutanota.com>
2025-05-20 00:06:19 +0200

    bugfix

--- server/actors/actor.py
+++ server/actors/actor.py
@@ -397,12 +397,12 @@ class Actor:
 
     
     def set_turn(self):
-        '''
+        
         if self.room == None:
             return
         if self.room.combat == None:
             return
-        '''
+        
         
         if type(self).__name__ == "Player":
             output = f'@yellowYour turn.@normal'
