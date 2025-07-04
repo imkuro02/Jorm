de7f858a141d1d032c20437e23f8903c16f39585
ignacy kurowski <ignacykurowski@tutanota.com>
2025-07-04 02:23:03 +0200

    editor QOL

--- jormedit/gui_node.js
+++ jormedit/gui_node.js
@@ -35,11 +35,28 @@ const nodeIdInput = document.createElement("input");
 nodeGroup.appendChild(nodeIdInput);
 nodeGroup.appendChild(br());
 
+const nodeSaveIdInput = document.createElement("input");
+nodeSaveIdInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveIdInput);
+nodeGroup.appendChild(label('save_id'));
+nodeGroup.appendChild(br());
+
+
+
 nodeGroup.appendChild(label('label'));
 nodeGroup.appendChild(br());
 const nodeLabelInput = document.createElement("input");
 nodeGroup.appendChild(nodeLabelInput);
 nodeGroup.appendChild(br());
+
+const nodeSaveLabelInput = document.createElement("input");
+nodeSaveLabelInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveLabelInput);
+nodeGroup.appendChild(label('save_label'));
+nodeGroup.appendChild(br());
+
+
+
 
 nodeGroup.appendChild(label('color'));
 nodeGroup.appendChild(br());
@@ -47,11 +64,29 @@ const nodeColorInput = document.createElement("input");
 nodeGroup.appendChild(nodeColorInput);
 nodeGroup.appendChild(br());
 
+const nodeSaveColorInput = document.createElement("input");
+nodeSaveColorInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveColorInput);
+nodeGroup.appendChild(label('save_color'));
+nodeGroup.appendChild(br());
+
+
+
+
 nodeGroup.appendChild(label('name'));
 nodeGroup.appendChild(br());
 const nodeNameInput = document.createElement("input");
 nodeGroup.appendChild(nodeNameInput);
 nodeGroup.appendChild(br());
+
+const nodeSaveNameInput = document.createElement("input");
+nodeSaveNameInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveNameInput);
+nodeGroup.appendChild(label('save_name'));
+nodeGroup.appendChild(br());
+
+
+
 
 nodeGroup.appendChild(label('desc'));
 nodeGroup.appendChild(br());
@@ -59,11 +94,29 @@ const nodeDescInput = document.createElement("textarea");
 nodeGroup.appendChild(nodeDescInput);
 nodeGroup.appendChild(br());
 
+const nodeSaveDescInput = document.createElement("input");
+nodeSaveDescInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveDescInput);
+nodeGroup.appendChild(label('save_desc'));
+nodeGroup.appendChild(br());
+
+
+
+
 const nodeCanBeRecallSiteInput = document.createElement("input");
 nodeCanBeRecallSiteInput.type = 'checkbox';
 nodeGroup.appendChild(nodeCanBeRecallSiteInput);
 nodeGroup.appendChild(label('safe'));
 nodeGroup.appendChild(br());
+
+const nodeSaveSafeInput = document.createElement("input");
+nodeSaveSafeInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveSafeInput);
+nodeGroup.appendChild(label('save_safe'));
+nodeGroup.appendChild(br());
+
+
+
 
 const nodeInstancedInput = document.createElement("input");
 nodeInstancedInput.type = 'checkbox';
@@ -71,17 +124,45 @@ nodeGroup.appendChild(nodeInstancedInput);
 nodeGroup.appendChild(label('instanced'));
 nodeGroup.appendChild(br());
 
+const nodeSaveInstancedInput = document.createElement("input");
+nodeSaveInstancedInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveInstancedInput);
+nodeGroup.appendChild(label('save_instanced'));
+nodeGroup.appendChild(br());
+
+
+
+
 const nodeDoorwayInput = document.createElement("input");
 nodeDoorwayInput.type = 'checkbox';
 nodeGroup.appendChild(nodeDoorwayInput);
 nodeGroup.appendChild(label('doorway'));
 nodeGroup.appendChild(br());
 
+const nodeSaveDoorwayInput = document.createElement("input");
+nodeSaveDoorwayInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveDoorwayInput);
+nodeGroup.appendChild(label('save_doorway'));
+nodeGroup.appendChild(br());
+
+
+
+
+
 nodeGroup.appendChild(label('spawner syntax'));
 nodeGroup.appendChild(br());
 const nodeSpawnGroupInput = document.createElement("textarea");
 nodeGroup.appendChild(nodeSpawnGroupInput);
 nodeGroup.appendChild(br());
+
+const nodeSaveSpawnGroupInput = document.createElement("input");
+nodeSaveSpawnGroupInput.type = 'checkbox';
+nodeGroup.appendChild(nodeSaveSpawnGroupInput);
+nodeGroup.appendChild(label('save_spawngroup'));
+nodeGroup.appendChild(br());
+
+
+
 
 nodeFormContainer.appendChild(nodeGroup);
 
@@ -159,6 +240,18 @@ function save_json() {
 
     };
 
+    if (!nodeSaveIdInput.checked){delete data['id'];}
+    if (!nodeSaveLabelInput.checked){delete data['label'];}
+    if (!nodeSaveColorInput.checked){delete data['color'];}
+    if (!nodeSaveNameInput.checked){delete data['name'];}
+    if (!nodeSaveDescInput.checked){delete data['description'];}
+    if (!nodeSaveSafeInput.checked){delete data['safe'];}
+    if (!nodeSaveInstancedInput.checked){delete data['instanced'];}
+    if (!nodeSaveDoorwayInput.checked){delete data['doorway'];}
+    if (!nodeSaveSpawnGroupInput.checked){delete data['spawner'];}
+
+
+    /*
     for (const key in data) {
         if (data.hasOwnProperty(key)) {
             if (data[key] === false || data[key] === '' || (typeof data[key] === "undefined")) {
@@ -166,6 +259,10 @@ function save_json() {
             }
         }
     }
+    */
+
+    
+
     //console.log(JSON.stringify(data, null, 2));
 
     const textToCopy = JSON.stringify(data, null, 2);
@@ -179,8 +276,15 @@ function save_json() {
     //    });
 
     cy.nodes(':selected').forEach(function(node) {
-        // Your code to operate on each selected node
-        node.data().json = data
+        const nodeData = node.data().json;
+        
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                nodeData[key] = data[key];
+            }
+        }
+    
+        node.data(nodeData);
     });
     //if (window.selectedNode!=null){
     //    window.selectedNode.data().json = data
