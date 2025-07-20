1aec911d52040efbb081a1bf267dffc05a06a116
ignacy kurowski <ignacykurowski@tutanota.com>
2025-07-21 00:14:00 +0200

    small bugfish

--- godot_client/scripts/main.gd
+++ godot_client/scripts/main.gd
@@ -46,7 +46,7 @@ func _ready():
 	INPUT.text_submitted.connect(_on_input_submitted)
 
 	# Connect to WebSocket server
-	var err = socket.connect_to_url(websocket_url_local)
+	var err = socket.connect_to_url(websocket_url)
 	if err != OK:
 		print("Unable to connect")
 		set_process(false)
