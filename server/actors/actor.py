56b6087db0cf581357ea9bc809b3d76f90ed2a6a
Ignacy <ignacy@kurowski.xyz>
2025-11-21 09:15:28 +0100

    stat rebalance

--- server/actors/actor.py
+++ server/actors/actor.py
@@ -49,28 +49,28 @@ class ActorStatManager:
         match stat:
             case StatType.GRIT:
                 stat = StatType.GRIT
-                hp_bonus = 10
+                hp_bonus = 9
                 mp_bonus = 3
-                pa_bonus = 5
-                ma_bonus = 1
+                pa_bonus = 0
+                ma_bonus = 0
             case StatType.FLOW:
                 stat = StatType.FLOW
-                hp_bonus = 5
-                mp_bonus = 3
-                pa_bonus = 10
-                ma_bonus = 1
+                hp_bonus = 8
+                mp_bonus = 4
+                pa_bonus = 0
+                ma_bonus = 0
             case StatType.MIND:
                 stat = StatType.MIND
-                hp_bonus = 1
-                mp_bonus = 10
-                pa_bonus = 3
-                ma_bonus = 5
+                hp_bonus = 4
+                mp_bonus = 8
+                pa_bonus = 0
+                ma_bonus = 0
             case StatType.SOUL:
                 stat = StatType.SOUL
-                hp_bonus = 3
-                mp_bonus = 5
-                pa_bonus = 1
-                ma_bonus = 10
+                hp_bonus = 6
+                mp_bonus = 6
+                pa_bonus = 0
+                ma_bonus = 0
             case _:
                 hp_bonus = 0
                 mp_bonus = 0
