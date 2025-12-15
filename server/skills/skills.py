d11bcdd9fcd10269d8a78531bf84225d304e82cd
Ignacy <ignacy@kurowski.xyz>
2025-12-15 14:11:39 +0100

    fixed bleeding affect description

--- server/skills/skills.py
+++ server/skills/skills.py
@@ -449,7 +449,7 @@ class SkillDamageByFlowApplyBleed(SkillDamage):
         stunned_affect = affects.AffectBleed(
             affect_source_actor = self.user,
             affect_target_actor = self.other,
-            name = 'Bleeding', description = f'{bleed_damage} Physical damage per turn until healed',
+            name = 'Bleeding', description = f'Receive physical damage each turn until healed',
             turns = self.script_values['duration'][self.users_skill_level],
             resisted_by = StatType.PHYARMOR,
             damage = bleed_damage,
