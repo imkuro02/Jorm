058fc5fd9d0e435522b6b269226c9011b80beb90
Ignacy Kurowski <ignacy@kurowski.xyz>
2026-02-03 07:27:51 +0100

    eh load speedup

--- server/systems/world.py
+++ server/systems/world.py
@@ -265,6 +265,8 @@ class World:
 
             # check if you actually want some other class
             room_class = custom_loader.compare_replace_rooms(self.rooms[r])
+            if room_class.__name__ == self.rooms[r].__class__.__name__:
+                continue
             self.rooms_to_unload.append(r)
             self.unload_rooms()
 
