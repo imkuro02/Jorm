be1fd478c052c80f361749b904e301d6f171bb2c
Ignacy Kurowski <ignacy@kurowski.xyz>
2026-09-06 20:48:18 +0200

    mute

--- server/systems/utils.py
+++ server/systems/utils.py
@@ -46,6 +46,7 @@ from systems.utils import show_caller
 '''
 
 def debug_print(*args, **kwargs):
+    return
     stack = traceback.extract_stack()
     filename, lineno, func, _ = stack[-2]
     print(f"[{filename}:{lineno} in {func}()]", *args, **kwargs)
