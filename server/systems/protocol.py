59e49086621143f6f1164ad6f969a54e20cdb970
Ignacy Kurowski <ignacy@kurowski.xyz>
2026-09-06 23:10:27 +0200

    abort

--- server/systems/protocol.py
+++ server/systems/protocol.py
@@ -892,8 +892,8 @@ This ONE TIME password will not work next time you try to log in.{Color.NORMAL}
             self.unload_actor()
 
         self.factory.protocols.remove(self)
-        self.transport.loseConnection()
-        print(self.transport._tempDataBuffer)
+        self.transport.abortConnection()
+        #print(self.transport._tempDataBuffer)
 
 
     # override
