af99768604abbe1f2547766fbb3f93e0f00794a2
Ignacy Kurowski <ignacy@kurowski.xyz>
2026-09-07 14:21:58 +0200

    less often db sync

--- server/__main__.py
+++ server/__main__.py
@@ -53,7 +53,7 @@ class ServerFactory(protocol.Factory):
         # for room in self.world.rooms.values():
         #     room.tick()
 
-        if self.ticks_passed % (TICKRATE * 3 * 1) == 0 or self.ticks_passed == 10:
+        if self.ticks_passed % (TICKRATE * 60 * 3) == 0 or self.ticks_passed == 10:
             for i in self.protocols:
                 if i.actor != None:
                     # self.db.write_actor(i.actor)
