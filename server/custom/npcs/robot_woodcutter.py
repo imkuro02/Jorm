3a160c1bca4a4eb18776a880f945d72d1eefab64
Ignacy Kurowski <ignacy@kurowski.xyz>
2026-09-16 12:16:19 +0200

    woodcutter bot

--- server/custom/npcs/robot_woodcutter.py
+++ server/custom/npcs/robot_woodcutter.py
@@ -10,16 +10,28 @@ class robot_woodcutter(Npc):
             return False
         return True
 
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.wander_directions_order = ['south', 'north']
+        self.wander_directions_order = ['north', 'south']
+        self.name = self.name.replace('The','The Wandering')
         self.wander_direction_current = 0
         self.wander_ticks_passed = 0
-        self.wander_ticks_required = TICKRATE*3
+        self.wander_ticks_required = TICKRATE*10
         self.wander_ticks_warning_required = 10
         self.footprints = []
         self.pre_footprints = []
         self.footprints_max = 1
+
+        #self.trigger_manager.trigger_add(trigger_key = 'command_go', trigger_action = self.trigger_dont_leave)
+        #self.trigger_manager.trigger_add(trigger_key = 'command_rest', trigger_action = self.trigger_dont_leave)
+
+    def trigger_dont_leave(self, player, line):
+        if player.status == ActorStatusType.DEAD:
+            return False
+
+        player.send_line(f'You cannot do that while {self.pretty_name(identifier = player)} is here')
+        return True
 
     def despawn_footprints(self):
         for i in self.footprints:
@@ -38,8 +50,8 @@ class robot_woodcutter(Npc):
     def spawn_footprints(self, dir):
         corpse = Item()
         corpse.name = f'Footprints'
-        corpse.description = f'There is a set of tire tracks leading {dir.lower()}, you wonder who they belong to.'
-        corpse.description_room = f'{Color.BAD}A pair of tire tracks is leading {dir.lower()}{Color.BACK}'
+        corpse.description = f'There is a set of tire tracks leading {dir.lower()}, you wonder what they belong to.'
+        corpse.description_room = f'{Color.BAD}A pair of tire track is leading {dir.lower()}{Color.BACK}'
         corpse.stack_max = 1
         corpse.keep = False
         corpse.can_pick_up = False
@@ -50,10 +62,10 @@ class robot_woodcutter(Npc):
         self.room.inventory_manager.add_item(corpse)
         self.footprints.append(corpse)
 
-        list_pretty_name_objects = [self]
         for i in self.room.actors.values():
+            list_pretty_name_objects = [self]
             br = f'{self.id} {Color.BAD}leaves a trail of tire tracks behind.{Color.BACK}'
-            i.pretty_broadcast(br, br, list_pretty_name_objects = list_pretty_name_objects)
+            i.pretty_broadcast(br,br, list_pretty_name_objects = list_pretty_name_objects)
             break
 
         if len(self.footprints)-1>=self.footprints_max:
@@ -62,38 +74,43 @@ class robot_woodcutter(Npc):
                 return
 
             for i in self.footprints[0].inventory_manager.owner.actors.values():
-                list_pretty_name_objects = [self]
                 br = f'{Color.BAD}The tire tracks leading {dir} dry up and the trail goes cold.{Color.BACK}'
-                i.pretty_broadcast(br, br, list_pretty_name_objects = list_pretty_name_objects)
+                i.simple_broadcast(br,br)
                 break
 
             self.footprints[0].inventory_manager.remove_item(self.footprints[0])
             self.footprints[0].unload()
             self.footprints.pop(0)
 
+
         self.despawn_pre_footprints()
         
         
 
     def spawn_pre_footprints(self, _exit):
-        corpse = Item()
-        corpse.name = _exit.direction
-        corpse.description = f'{Color.BAD}There is something approaching from {_exit.direction}{Color.BACK}'
-        corpse.description_room = f'{Color.BAD}There is something approaching from {_exit.direction}{Color.BACK}'
-        corpse.stack_max = 1
-        corpse.keep = False
-        corpse.can_pick_up = False
-        corpse.footprint_leading_dir = _exit.direction
-        corpse.invisible = True
-        corpse.premade_id = 'footprints_something_blabla1'
-        
-        _exit.room.inventory_manager.add_item(corpse)
-        self.pre_footprints.append(corpse)
+        try:
+            corpse = Item()
+            corpse.name = _exit.direction
+            corpse.description = f'{Color.BAD}There is something approaching from {_exit.direction}{Color.BACK}'
+            corpse.description_room = f'{Color.BAD}There is something approaching from {_exit.direction}{Color.BACK}'
+            corpse.stack_max = 1
+            corpse.keep = False
+            corpse.can_pick_up = False
+            corpse.footprint_leading_dir = _exit.direction
+            corpse.invisible = True
+            corpse.premade_id = 'footprints_something_blabla1'
+            
+            _exit.room.inventory_manager.add_item(corpse)
+            self.pre_footprints.append(corpse)
 
-        if len(self.pre_footprints)-1>=1:
-            self.pre_footprints[0].inventory_manager.remove_item(self.pre_footprints[0])
-            self.pre_footprints[0].unload()
-            self.pre_footprints.pop(0)
+            if len(self.pre_footprints)-1>=1:
+                self.pre_footprints[0].inventory_manager.remove_item(self.pre_footprints[0])
+                self.pre_footprints[0].unload()
+                self.pre_footprints.pop(0)
+        except Exception as e:
+            from systems.utils import debug_print
+            debug_print(e)
+            print(e)
 
 
     def wander(self, warning = False):
@@ -129,9 +146,9 @@ class robot_woodcutter(Npc):
                     self.wander_direction_current = 0
                     
     def tick(self):
+        super().tick()
         from scripts.greet_message import greet_message
         greet_message(self, f'{self.id} says "You are in the way"')
-        super().tick()
         self.wander_ticks_passed += 1
         if self.wander_ticks_passed == self.wander_ticks_warning_required:
             self.wander(warning = True)
@@ -146,4 +163,9 @@ class robot_woodcutter(Npc):
 
     def unload(self):
         super().unload()
+
+from actors.npcs import Npc
+from configuration.constants.actor_status_type import ActorStatusType
+from configuration.constants.color import Color
+from items.misc import Item
 
